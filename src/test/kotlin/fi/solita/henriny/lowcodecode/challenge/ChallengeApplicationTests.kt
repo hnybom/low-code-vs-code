5efc5cd652b810c70efe8f383ced9f2af917b6ea
Henri Nybom <henri.nybom@solita.fi>
2021-03-17 08:58:41 +0200

    Initial commit

---
+++ src/test/kotlin/fi/solita/henriny/lowcodecode/challenge/ChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package fi.solita.henriny.lowcodecode.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

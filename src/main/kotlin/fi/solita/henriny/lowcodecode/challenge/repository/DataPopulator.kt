c4190d8db25782c11b670ce61b8ede4aed0ca365
Henri Nybom <henri.nybom@solita.fi>
2021-04-01 12:17:31 +0300

    Add initial data population at the start

---
+++ src/main/kotlin/fi/solita/henriny/lowcodecode/challenge/repository/DataPopulator.kt
@@ -0,0 +1,56 @@
+package fi.solita.henriny.lowcodecode.challenge.repository
+
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import com.github.javafaker.Faker
+import fi.solita.henriny.lowcodecode.challenge.repository.model.Game
+import fi.solita.henriny.lowcodecode.challenge.repository.model.HighScore
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import org.springframework.util.ResourceUtils
+import java.time.ZoneId
+import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+
+@Component
+class GamePopulator(val gameRepository: GameRepository) {
+
+    @EventListener
+    fun onApplicationEvent(event: ContextRefreshedEvent) {
+        if (gameRepository.count() == 0L) return
+        val faker = Faker()
+
+        csvReader().open(ResourceUtils.getFile("classpath:video_games.csv")) {
+            readAllAsSequence().filterIndexed { index, _ -> index != 0 }.map { row: List<String> ->
+                Game(
+                    id = null,
+                    version = null,
+                    name = row[0],
+                    platform = row[12],
+                    genre = row[5],
+                    publisher = row[7],
+                    rating = row[13],
+                    releaseYear = row[15].toInt(),
+                    reviewScore = row[9].toInt(),
+                    highScores = (0..Random.nextInt(100)).map {
+                        HighScore(
+                            id = null,
+                            version = null,
+                            game = null,
+                            gamerName = faker.name().fullName(),
+                            score = Random.nextLong(9999999L),
+                            created = faker.date().past(1200, 10, TimeUnit.DAYS).toInstant()
+                                .atZone(ZoneId.systemDefault()).toLocalDateTime()
+                        )
+                    }.toSet()
+                )
+            }.forEach {
+                gameRepository.save(it)
+            }
+        }
+        
+    }
+
+}
--- src/test/kotlin/fi/solita/henriny/lowcodecode/challenge/populate/PopulateDB.kt
+++ src/test/kotlin/fi/solita/henriny/lowcodecode/challenge/populate/PopulateDB.kt
@@ -25,36 +25,6 @@ class PopulateDB {
 
     @Test
     fun populate() {
-        if(gameRepository.count() >0) return
 
-        val faker = Faker()
-
-        csvReader().open(ResourceUtils.getFile("classpath:video_games.csv")) {
-            readAllAsSequence().filterIndexed { index, _ -> index != 0 }.map { row : List<String> ->
-                Game(
-                    id = null,
-                    version = null,
-                    name = row[0],
-                    platform = row[12],
-                    genre = row[5],
-                    publisher = row[7],
-                    rating = row[13],
-                    releaseYear = row[15].toInt(),
-                    reviewScore = row[9].toInt(),
-                    highScores = (0..Random.nextInt(100)).map {
-                        HighScore(
-                            id = null,
-                            version = null,
-                            game = null,
-                            gamerName = faker.name().fullName(),
-                            score = Random.nextLong(9999999L),
-                            created = faker.date().past(1200, 10, TimeUnit.DAYS).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
-                        )
-                    }.toSet()
-                )
-            }.forEach {
-                gameRepository.save(it)
-            }
-        }
     }
 }
